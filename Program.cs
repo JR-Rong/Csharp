ccda10f8687567a4d3db992ce61bdd84816074a6
JR-Rong <73985642+JR-Rong@users.noreply.github.com>
2020-12-04 16:22:54 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,276 +4,275 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace day02
+namespace day03
 {
     class Program
     {
+        static Random random = new Random();
         static void Main1(string[] args)
         {
-            string gunname = "AK47";
-            string ammoCapcity = "30";
-            
-            string str = 
-                string.Format("枪名称为：{0}, 剩余容量为：{1}", gunname, ammoCapcity);
-            Console.WriteLine(str);
-            Console.WriteLine("{0:d2}", 5);
-            Console.WriteLine("{0:f2}", 1.33);
-            Console.WriteLine("{0:p1}", 0.1);
-            Console.WriteLine("i love \"unity\"!");
-            Console.WriteLine("i love \n\t!");
-
+            Console.WriteLine("input num");
+            int res = Add_and_Subtract(int.Parse(Console.ReadLine()));
+            Console.WriteLine("result is:" + res);
             Console.ReadLine();
+        }
+
+        private static int Add_and_Subtract(int i)
+        {
+            if (i == 1) return 1;
+            if (i % 2 == 0)
+            {
+                return -i + Add_and_Subtract(i - 1); 
+            }
+            else
+            {
+                return i + Add_and_Subtract(i - 1);
+            }
         }
 
         static void Main2()
         {
-            float n1 = 5, n2 = 2;
-            float res = n1 / n2;
-            int a = 1;
-            string b = a > 2 ? "yes" : "no";
-            Console.WriteLine(b);
+            Console.WriteLine("请输入学生人数");
+            int count = int.Parse(Console.ReadLine());
+            float[] scores = GetScore(count);
+            for (int i = 0; i< scores.Length; i++)
+            {
+                Console.WriteLine(scores[i]);
+            }
+            
             Console.ReadLine();
+        }
 
+        private static float[] GetScore(int count)
+        {
+            float[] scores = new float[count];
+            for (int i = 0; i<scores.Length; )
+            {
+                Console.WriteLine("请输入第{0}个学生成绩", i+1);
+                float score = float.Parse(Console.ReadLine());
+
+                if (score >= 0 && score <= 100)
+                {
+                    scores[i++] = score;
+                }
+                else
+                {
+                    Console.WriteLine("成绩输入有误，请重新输入");
+                }
+                
+            }
+            return scores;
         }
 
         static void Main3()
         {
-            //数据类型转换
-            //string num = "19";
-            //int num1 = int.Parse(num);
-            //float num2 = float.Parse(num);
-
-            //string str = num2.ToString();
-
-            Console.WriteLine("请输入一个4位数字：");
-            string user_Input_Num = Console.ReadLine();
-            int res = int.Parse(user_Input_Num[0].ToString());
-            res += int.Parse(user_Input_Num[1].ToString());
-            res += int.Parse(user_Input_Num[2].ToString());
-            res += int.Parse(user_Input_Num[3].ToString());
-            Console.WriteLine("结果为：{0}", res);
-            Console.ReadLine();
-
-        }
-
-        static void Main4()
-        {
-            //隐式转换
-            byte n1 = 100;
-            int i1 = n1;
-
-            int n2 = 100;
-            byte i2 = (byte)n2;
-
-        }
-
-        static void Main5()
-        {
-            Console.WriteLine("请输入性别：");
-            string sex = Console.ReadLine();
-            if (sex == "男")
+            int[] a = new int[2] { 1, 2 };
+            int[] b = { 1, 2, 3 };
+            //var可以自动判断数据类型如 var a = 1，var b = 2.0
+            //array类型为数组的父类，各数组类型为子类，如int[],float[]...
+            //子类可以幅值给父类，如array a = int[], array b = float[]
+            //object为万类之父
+            //数组操作：数组名.Length, Array.Clare, Array.copy, 数组名.CopyTo, 数组名.Clone, Array.IndexOf, Array.LastIndexOf, Array.Soft, Array.Reverse
+            foreach (int item in new int[3] {1, 2, 3 })
             {
-                Console.WriteLine("nan");
+                Console.WriteLine(item);
             }
-            else if (sex == "女")
-            {
-                Console.WriteLine("nv");
-            }
-            else
-            {
-                Console.WriteLine("unknow");
-            }
-
-        }
-
-        static void Main6()
-        {
-            int res;
-            Console.WriteLine("输入第一个数");
-            int num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("输入第二个数");
-            int num2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("输入一个符号");
-            string op = Console.ReadLine();
-
-            switch (op)
-            {
-                case "+":
-                    res = num1 + num2;
-                    break;
-                case "-":
-                    res = num1 - num2;
-                    break;
-                case "*":
-                    res = num1 * num2;
-                    break;
-                case "/":
-                    res = num1 / num2;
-                    break;
-                default:
-                    res = -1;
-                    break;
-            }
-
-        }
-
-        static void Main7()
-        {
-            int i = 0;
-            float height = 100, distance = 0;
-            for (; (height/2) >= 0.01; i++)
-            {
-                distance += height;
-                height /= 2;
-                distance += height;
-            }
-            distance += height;
-            Console.WriteLine("共弹了{0}次，共弹了{1}米", i, distance);
-            Console.ReadLine();
-
-        }
-
-        static void Main8()
-        {
-            //猜随机数
-            Random random = new Random();
-            int num = random.Next(0, 101);
-            Console.WriteLine("请输入一个数");
-            int user_num = int.Parse(Console.ReadLine());
-            int i = 1;
-            while (user_num != num)
-            {
-                if (user_num > num)
-                {
-                    Console.WriteLine("大了");
-                    user_num = int.Parse(Console.ReadLine());
-                }
-                else if (user_num < num)
-                {
-                    Console.WriteLine("小了");
-                    user_num = int.Parse(Console.ReadLine());
-                }
-                i++;
-            }
-            Console.WriteLine("猜对了！,猜了{0}次", i);
             Console.ReadLine();
         }
 
-        private static void Fun1()
-        {
-            Console.WriteLine("fun1执行了");
-            Console.ReadLine();
-        }
-
-        static void Main9()
-        {
-            Fun1();
-        }
-
-
+        //彩票生成器
         static void Main()
         {
-            Console.WriteLine("请输入年份：");
-            int year = int.Parse(Console.ReadLine());
-            Print_Year(year);
-            Console.ReadLine();
-        }
-        /// <summary>
-        /// 根据年月日得到星期几的方法
-        /// </summary>
-        /// <param name="year"></param>
-        /// <param name="month"></param>
-        /// <param name="day"></param>
-        /// <returns></returns>
-        private static int GetWeekByDay(int year, int month, int day)
-        {
-            DateTime dt = new DateTime(year, month, day);
-            return (int)dt.DayOfWeek;
-        }
+            bool flag = true;
+            int total_award = 50000;
+            int count = 0;
+            int total_win = 0;
+            Console.WriteLine("双色球规则：红球6个，号码为1-33，蓝球1个，号码为1-16");
 
-        private static int Get_Daynum_Of_month(int month, int flag = 0)
-        {
-            switch (month)
-            {
-                case 2:
-                    if (flag == 1)
-                    {
-                        return 29;
-                    }
-                    else
-                    {
-                        return 28;
-                    }
-                    break;
-                case 1:
-                case 3:
-                case 5:
-                case 7:
-                case 8:
-                case 10:
-                case 12:
-                    return 31;
-                    break;
-                default:
-                    return 30;
-                    break;
-            }
-        }
+            Console.WriteLine("请输入投注数（2元一注，最少一注）：");
+            int money = int.Parse(Console.ReadLine());
+            total_award += 2 * money;
 
-        private static int Judge_Run_Year(int year)
-        {
-            if (year % 4 != 0)
+            Console.WriteLine("\n本期奖池总奖金为：{0}\n", total_award);
+
+            int[] Red_ball = new int[6];
+            int blue_ball = 0;
+
+            Red_ball = BuyRedBall(Red_ball);
+            blue_ball = BuyBlueBall();
+            int[] buy_ball = ConcatBalls(Red_ball, blue_ball); 
+
+            Console.WriteLine("\n购买的球为");
+            for (int i = 0; i < buy_ball.Length; i++)
             {
-                return 0;
+                Console.Write("{0}\t", buy_ball[i]);
             }
-            else
+
+            while (flag)
             {
-                if (year % 400 != 0 && year % 100 == 0)
+                Console.WriteLine("\n\n第{0}次開獎中.........\n", count+1);
+                int[] balls = GenerateBall();
+
+                Console.WriteLine("\n中奖号码为");
+                for (int i = 0; i < balls.Length; i++)
                 {
-                    return 0;
+                    Console.Write("{0}\t", balls[i]);
+                }
+
+                Console.WriteLine();
+
+                int award = LevelCountAward(WinMoneyLevel(buy_ball, balls, total_award), money, total_award);
+
+                if (award > 0)
+                {
+                    Console.WriteLine("恭喜你獲得了{0}元獎金", award);
                 }
                 else
                 {
-                    return 1;
+                    Console.WriteLine("謝謝參與！\n");
+                }
+
+                total_win += award;
+
+                count++;
+
+                if (count >= money)
+                { 
+                    break;
+                }
+
+                //Console.WriteLine("是否繼續抽獎?(Y/N)");
+                //string cont_flag = Console.ReadLine();
+                //if (cont_flag == "N" || cont_flag == "n")
+                //{
+                //    break;
+                //}
+                //else if(cont_flag == "Y" || cont_flag == "y" || cont_flag == "")
+                //{
+                //    count++;
+                //}     
+                
+            }
+            Console.WriteLine("開獎結束");
+            Console.WriteLine("共花費{0}元,賺了{1}元", count * 2, total_win);
+            Console.ReadLine();
+
+        }
+
+        private static int[] BuyRedBall(int[] Red_ball)
+        {
+            int temp=0;
+            for (int i = 0; i < Red_ball.Length;)
+            {
+                Console.WriteLine("请输入第{0}个红球号码", i+1);
+                temp = int.Parse(Console.ReadLine());
+                if (temp > 0 && temp < 34 && !(Array.IndexOf(Red_ball, temp) >= 0))
+                    Red_ball[i++] = temp;
+                else
+                    Console.WriteLine("请重新输入");
+            }
+            Array.Sort(Red_ball);
+            return Red_ball;
+        }
+
+        private static int BuyBlueBall()
+        {
+            Console.WriteLine("请输入蓝球号码");
+            return int.Parse(Console.ReadLine());
+        }
+
+        private static int[] GenerateBall()
+        {
+            int[] redball = new int[6];
+            int[] ball = new int[7];
+            for (int i = 0; i < redball.Length;)
+            {
+                int temp = random.Next(1, 34);
+                if (Array.IndexOf(redball, temp) < 0)
+                {
+                    redball[i++] = temp;
                 }
             }
+            Array.Sort(redball);
+            int blueball = random.Next(1, 17);
+            redball.CopyTo(ball, 0);
+            ball[6] = blueball;
+            return ball;
         }
 
-        private static void Print_Month(int year, int month)
+        private static int[] ConcatBalls(int[] red, int blue)
         {
-            Console.WriteLine("{0}年{1}月", year, month);
-            Console.WriteLine();
-            Console.WriteLine("日\t一\t二\t三\t四\t五\t六");
-            for (int i=0; i< GetWeekByDay(year, month, 1); i++)
-            {
-                Console.Write("\t");
-            }
-
-            for (int i = 1; i<=Get_Daynum_Of_month(month, Judge_Run_Year(year)); i++)
-            {
-                Print_Day(year, month, i);
-            }
-            Console.WriteLine();
-            Console.WriteLine();
+            int[] balls = new int[7];
+            red.CopyTo(balls, 0);
+            balls[6] = blue;
+            return balls;
         }
 
-        private static void Print_Day(int year, int month, int day)
+        private static int WinMoneyLevel(int[] buy, int[] goal, int money)
         {
-            Console.Write("{0}\t", day);
-            if (GetWeekByDay(year, month, day) == 6)
+            int[] red = new int[buy.Length - 1];
+            Array.Copy(buy, 0, red, 0, 6);
+            int red_hit = 0, blue_hit = 0;
+            for (int i = 0; i < red.Length; i++)
             {
-                Console.WriteLine();
-                Console.WriteLine();
+                if (Array.IndexOf(red, goal[i]) >= 0)
+                {
+                    red_hit++;
+                }
+            }
+
+            if (goal[6] == buy[6]) blue_hit++; 
+
+            if (red_hit == 6 && blue_hit == 1)
+            {
+                return 1;
+            }
+            else if (red_hit == 6 && blue_hit == 0)
+            {
+                return 2;
+            }
+            else if (red_hit == 5 && blue_hit == 1)
+            {
+                return 3;
+            }
+            else if ((red_hit == 5 && blue_hit == 0) || (red_hit == 4 && blue_hit == 1))
+            {
+                return 4;
+            }
+            else if ((red_hit == 4 && blue_hit == 0) || (red_hit == 3 && blue_hit == 1))
+            {
+                return 5;
+            }
+            else if (red_hit < 3 && blue_hit == 1)
+            {
+                return 6;
+            }
+            else
+            {
+                return 0;
             }
         }
 
-        private static void Print_Year(int year)
+        private static int LevelCountAward(int level, int money, int total)
         {
-            for (int i=1; i<=12; i++)
+            switch (level)
             {
-                Print_Month(year, i);
+                case 1:
+                    return (int)(((float)total - 3215) * 0.7);
+                case 2:
+                    return (int)(((float)total - 3215) * 0.3);
+                case 3:
+                    return 3000;
+                case 4:
+                    return 200;
+                case 5:
+                    return 10;
+                case 6:
+                    return 5;
+                default:
+                    return 0;
             }
+              
         }
-
     }
 }
