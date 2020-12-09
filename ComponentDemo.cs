2a91220e3ee11cea93c1a81cccbf1e544ae3187d
JR-Rong <73985642+JR-Rong@users.noreply.github.com>
2020-12-09 11:57:10 +0800

    Add files via upload

---
+++ ComponentDemo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///component类提供了查找组件的功能，在当前物体、后代、祖先查找组件
+/// </summary>
+
+public class ComponentDemo : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        if (GUILayout.Button("点我"))
+        {
+            print("ok");
+            this.transform.position = new Vector3(1, 2, 3);
+            this.GetComponent<MeshRenderer>().material.color = Color.red;
+            var allcomponent = this.GetComponents<Component>();
+            var children = this.GetComponentsInChildren<MeshRenderer>(); 
+            foreach(var item in children)
+            {
+                item.material.color = Color.green;
+            }
+        }
+    }
+
+}
---
+++ TransformDemo.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///tranform类提供了查找（爸、孩子）变换方法，改变位置角度方法
+/// </summary>
+
+public class TransformDemo : MonoBehaviour
+{
+
+    public Transform tf;
+    private void OnGUI()
+    {
+
+        if(GUILayout.Button("rotate"))
+        {
+            //foreach(Transform item in this.transform)
+            //{
+            //    print(item.name);
+            //}
+            this.transform.Translate(0,0,1,Space.World);
+
+            this.transform.Rotate(0, 10, 1, Space.Self);
+
+        }
+
+        if (GUILayout.RepeatButton("rotatearound"))
+        {
+            this.transform.RotateAround(Vector3.zero, Vector3.up, 5);
+        }
+
+        if (GUILayout.RepeatButton("root"))
+        {
+            Transform root1 = this.transform.root;
+        }
+
+        if (GUILayout.RepeatButton("parent"))
+        {
+            Transform parent01 = this.transform.parent;
+        }
+
+        if (GUILayout.RepeatButton("setparent"))
+        {
+            this.transform.SetParent(tf, false);
+        }
+
+        if (GUILayout.RepeatButton("light"))
+        {
+            GameObject gamelight = new GameObject();
+            Light light = gamelight.AddComponent<Light>();
+            light.color = Color.blue;
+            light.type = LightType.Point;
+
+        }
+
+        if (GUILayout.RepeatButton("fineEnemy"))
+        {
+            Enemy enemy = new Enemy();
+            enemy.HP = 100;
+            Enemy[] enemys = FindObjectsOfType<Enemy>();
+            foreach (Enemy item in enemys)
+            {
+                if (item.HP < enemy.HP)
+                    enemy = item;
+            }
+
+            print(enemy.name);
+        }
+
+    }
+
+}
